b57dff50c7c4a6ecb5c27bd7420d23ad3e386c12
Bimal Shrestha <bimalstha291@gmail.com>
2023-04-09 10:06:47 +0545

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/homepage/Homepage"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage/>} />
+          <Route path="/login" element={<h1>Login</h1>} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div>
+        <h1>Homepage</h1>
+        <button type="button" class="btn btn-primary">Button</button>
+        <img src="/assets/images/1.jpg" alt="" />
+    </div>
+  )
+}
+
+export default Homepage
