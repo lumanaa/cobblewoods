124e87f6dcd46b08c025e19bab67115e88d4887f
Bimal Shrestha <bimalstha291@gmail.com>
2023-04-25 16:03:04 +0545

    Form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Homepage from "./pages/homepage/Homepage"
+import Homepage from "./pages/homepage/Homepage";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import About from "./pages/about/About";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <>
       <Router>
+        <Navbar />
+
         <Routes>
-          <Route path="/" element={<Homepage/>} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/register" element={<Register/>} />
+          <Route path="/" element={<Homepage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+          {/* about */}
+          <Route path="/about" element={<About />} />
         </Routes>
       </Router>
     </>
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,13 +1,59 @@
-import React from 'react'
-import Navbar from '../../components/navbar/Navbar'
+import React from "react";
 
 const Register = () => {
   return (
-    <div>
-        <Navbar/>
-        <h1>Register</h1>
-    </div>
-  )
-}
+    <div className="container">
+      <div className="col-md-5"> 
+        <h1>Register a user</h1>
 
-export default Register
+        <form action="">
+          <div className="form-group">
+            <label htmlFor="name">Firstname</label>
+            <input type="text" name="name" id="name" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="lastname">Lastname</label>
+            <input
+              type="text"
+              name="lastname"
+              id="lastname"
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password2">Confirm Password</label>
+            <input
+              type="password"
+              name="password2"
+              id="password2"
+              className="form-control"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary mt-3 w-100">
+            Register
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
